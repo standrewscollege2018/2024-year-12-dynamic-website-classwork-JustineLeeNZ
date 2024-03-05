128180b3c5e32a147ed1a2392db51b6e24572d4c
JustineLeeNZ <jlu@stac.school.nz>
2024-03-05 14:06:58 +1300

    demos php basics
    covers echo, using HTML in an echo, how to set up links to use GET array, how to extract value from GET array

---
+++ php-intro/demo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo of PHP basics</title>
+</head>
+<body>
+    <h1>Demo of PHP Basics</h1>
+
+    <?php
+      echo "<h2>Hello world</h2>";  
+
+    ?>
+
+    <p><a href="get.php?number=1">Send a number to another page</a></p>
+    <p><a href="get.php?text='Mrs Lee is cool'">Send a string to another page</a></p>
+
+
+
+    
+</body>
+</html>
---
+++ php-intro/get.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $number_sent = $_GET['number'];
+        echo $number_sent;
+
+        $text_sent = $_GET['text'];
+        echo $text_sent;
+     ?>
+</body>
+</html>
